b9c0ba792d8cf445318c2567f47b0ba1bb353c31
Rexy-Gamaliel <rexygaming400@gmail.com>
2021-09-03 14:24:32 +0700

    Mau pull

--- app.py
+++ app.py
@@ -11,5 +11,16 @@ def home():
 def playfair_page():
     return render_template("playfair.html")
 
+@app.route('/vigenere', methods=["GET", "POST"])
+def vigenere_page():
+    print("Hello")
+    if request.method == "GET":
+        try:
+            mode = request.form["vMode"]
+        except:
+            mode = ""
+        print("Mode:", mode)
+    return render_template("vigenere.html", mode=mode)
+
 if __name__ == "__main__":
     app.run(debug=True)
