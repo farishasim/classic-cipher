0954922b833ddd6f304653ea935adfe220cc2288
Rexy-Gamaliel <rexygaming400@gmail.com>
2021-09-05 12:06:33 +0700

    Mau merge

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def home():
 def playfair_page():
     return render_template("playfair.html")
 
+@app.route('/playfair/encrypt')
 def playfair_encrypt():
     plain = request.args.get("text")
     key = request.args.get("key")
@@ -23,6 +24,36 @@ def playfair_decrypt():
     cipher = request.args.get("text")
     key = request.args.get("key")
     return playfair.decrypt(cipher, key)
+
+
+@app.route('/vigenere', methods=["GET", "POST"])
+def vigenere_page():
+    if request.method == "POST":
+        if request.form["vigenere_mode"] == "simpleVigenere":
+            return redirect("/vigenere/simple")
+        elif request.form["vigenere_mode"] == "autoKeyVigenere":
+            return redirect("/vigenere/autokey")
+        elif request.form["vigenere_mode"] == "fullVigenere":
+            return redirect("/vigenere/full")
+        elif request.form["vigenere_mode"] == "extendedVigenere":
+            return redirect("/vigenere/extended")
+    return render_template("vigenere.html")
+
+@app.route('/vigenere/simple')
+def simple_vigenere_page():
+    return render_template("simple_vigenere.html")
+
+@app.route('/vigenere/autokey')
+def auto_key_vigenere_page():
+    return render_template("auto_key_vigenere.html")
+
+@app.route('/vigenere/full')
+def full_vigenere_page():
+    return render_template("full_vigenere.html")
+
+@app.route('/vigenere/extended')
+def extended_vigenere_page():
+    return render_template("extended_vigenere.html")
 
 
 @app.route('/affine')
--- cipher/simpleVigenere.py
+++ cipher/simpleVigenere.py
@@ -1,6 +1,6 @@
 from processor import shiftAlphabet, formatInput
 
-def simpleVigenere():
+def simpleVigenereMain():
   p = formatInput(input("Input plainteks: "))
   k = formatInput(input("Input key: "))
   while (k == ""):
@@ -25,7 +25,38 @@ def simpleVigenere():
   print("Your ciphertext: " + c)
   print("Decrypted ciphertext: " + d)
 
-  # return c
+  # return {
+  #   "plaintext": p,
+  #   "key": k,
+  #   "ciphertext": c,
+  #   "decryptedtext": d
+  # }
 
-if __name__ == "main":
-  simpleVigenere()
+def simpleVigenereEncrypt(plaintext: str, keytext:str):
+  plaintext = formatInput(plaintext)
+  keytext = formatInput(keytext)
+  nk = len(keytext)
+
+  ciphertext = ""
+  i = 0
+  for letter in plaintext:
+    ciphertext += shiftAlphabet(letter, keytext[i % nk], '+')
+    i += 1
+  
+  return ciphertext
+
+def simpleVigenereDecrypt(ciphertext: str, keytext:str):
+  ciphertext = formatInput(ciphertext)
+  keytext = formatInput(keytext)
+  nk = len(keytext)
+
+  decryptedtext = ""
+  i = 0
+  for letter in ciphertext:
+    decryptedtext += shiftAlphabet(letter, keytext[i % nk], '-')
+    i += 1
+  
+  return decryptedtext
+
+if __name__ == "__main__":
+  simpleVigenereMain()
